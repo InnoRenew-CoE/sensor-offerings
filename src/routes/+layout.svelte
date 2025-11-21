7343086989d578450abc20cdb47e461c09f4008d
Mihael Berčič <mihael@regnum.si>
2025-11-21 23:43:31 +0100

    Color changes.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
-	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
-	
-	let { children } = $props();
+    import "../app.css";
+
+    let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+    <link rel="icon" href="./logo.svg" />
 </svelte:head>
 
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,7 +16,7 @@
 <div class="-mt-25 p-10 sm:p-30 py-40 bg-white" id="home">
     <div class="text-2xl text-indigo-500 font-departure">Sensors for Building Monitoring</div>
     <p class="text-lg w-full sm:w-[80%] py-10 px-5 text-gray-700">
-        Monitor your building like never before. Our service connects a variety of sensors—available directly from our site—and integrates seamlessly with a powerful data store, giving you real-time insights and actionable data to optimize building performance.
+        Monitor your building like never before. Our service connects a variety of sensor vailable directly from our sit nd integrates seamlessly with a powerful data store, giving you real-time insights and actionable data to optimize building performance.
     </p>
 
     <div class="border-dashed h-0.5 border-primary/50 border"></div>
@@ -33,13 +33,13 @@
     <div class="w-full flex flex-col gap-5 items-center justify-center">
         <!-- IAQ -->
         <div class="w-full">
-            <div class="py-5 grid gap-5 bg-[#8ABEB9] w-full">
+            <div class="py-5 grid gap-5 bg-[#213448] w-full">
                 <span id="iaq" class="text-white font-departure text-xl text-left m-auto w-[90%] pt-24">Indoor Air Quality sensor</span>
                 <div class="m-auto w-[80%] flex items-center justify-center">
                     <SensorCard fileLeft="iaq-transparent.png" fileRight="iaq.png" class="rounded-xl ">
                         <div class=" h-full p-3 text-white flex flex-wrap gap-5">
                             <div>
-                                <span class="font-departure text-[#48486F]">Supported Indicators:</span>
+                                <span class="font-departure text-[#fff]">Supported Indicators:</span>
                                 <ul class="list-disc pl-8 pt-3 space-y-1 text-white">
                                     <li>Carbon dioxide (CO₂)</li>
                                     <li>Volatile organic compounds (VOC)</li>
@@ -52,7 +52,7 @@
                                 </ul>
                             </div>
                             <div>
-                                <span class="font-departure text-[#48486F]">Short Description:</span>
+                                <span class="font-departure text-[#fff]">Short Description:</span>
                                 <div class="p-2">State-of-the-art wireless sensor measuring key indoor environmental factors. Powered by high-precision Sensirion sensors, it helps study how air quality affects health, comfort, and productivity, especially in workplaces.</div>
                             </div>
                         </div>
@@ -71,7 +71,7 @@
                     </p>
 
                     <p class="text-slate-700 leading-relaxed mb-4">
-                        Additionally, it plays a critical role in studying the impact of modern building materials—especially renewable and sustainable materials like wood—on indoor air quality, making it ideal for new wooden public buildings and research institutes experimenting with innovative
+                        Additionally, it plays a critical role in studying the impact of modern building material specially renewable and sustainable materials like woo n indoor air quality, making it ideal for new wooden public buildings and research institutes experimenting with innovative
                         construction materials. While our primary clients include public institutions and research facilities, the sensor is also well-suited for private homes seeking to optimize indoor environments.
                     </p>
 
@@ -82,13 +82,13 @@
 
         <!-- Sound -->
         <div class="w-full">
-            <div class="py-5 grid gap-5 bg-[#318AFC90] w-full">
+            <div class="py-5 grid gap-5 bg-[#070F2B] w-full">
                 <span id="sound" class="text-white m-auto font-departure text-xl text-left w-[90%] pt-24">Sound sensor</span>
                 <div class="m-auto w-[80%] flex items-center justify-center">
                     <SensorCard fileLeft="sound-transparent.png" fileRight="sound.png" class="rounded-xl ">
                         <div class=" h-full p-3 text-white flex flex-wrap">
                             <div>
-                                <span class="font-departure text-[#318afc]">Best for:</span>
+                                <span class="font-departure text-[#fff]">Best for:</span>
                                 <ul class="list-disc pl-6 space-y-2 pt-2">
                                     <li>Workplace noise monitoring to study stress and productivity</li>
                                     <li>Urban noise measurement for concerts, events, and city planning</li>
@@ -97,7 +97,7 @@
                                 </ul>
                             </div>
                             <div>
-                                <span class="font-departure text-[#318afc]">Description:</span>
+                                <span class="font-departure text-[#fff]">Description:</span>
                                 <div class="p-3">Wireless sensors measuring indoor/outdoor noise. One tracks decibels, the other adds on-device AI audio classification with privacy. Useful for workplace studies and urban noise monitoring.</div>
                             </div>
                         </div>
@@ -116,7 +116,7 @@
 
                     <p class="text-slate-700 leading-relaxed mb-4">
                         The first variant measures decibel levels alone, providing precise noise monitoring where sound pressure is the primary concern. The second includes an omnidirectional microphone that activates above 80 dB, capturing audio recordings processed on the device. Using Fast
-                        Fourier Transform (FFT), recordings are converted into a frequency spectrum for AI-driven noise source classification. Privacy is ensured—recordings never leave the device and are destroyed after classification.
+                        Fourier Transform (FFT), recordings are converted into a frequency spectrum for AI-driven noise source classification. Privacy is ensure ecordings never leave the device and are destroyed after classification.
                     </p>
 
                     <p class="text-slate-700 leading-relaxed mb-4">
@@ -129,20 +129,29 @@
 
         <!-- Outdoor Sound -->
         <div class="w-full">
-            <div class="py-5 grid gap-5 bg-[#00316F5a] w-full">
+            <div class="py-5 grid gap-5 bg-[#1E201E] w-full">
                 <span id="outdoor" class="text-white m-auto font-departure text-xl text-left w-[90%] pt-24">Outdoor Sound sensor</span>
                 <div class="m-auto w-[80%] flex items-center justify-center">
                     <SensorCard fileLeft="swarm-transparent.png" fileRight="swarm.png" class="rounded-xl ">
                         <div class=" h-full p-3 text-white flex flex-wrap lg:grid">
                             <div>
-                                <span class="font-departure text-[#00316F]">Best for:</span>
+                                <span class="font-departure text-[#fff]">Best for:</span>
                                 <ul class="list-disc pl-8 pt-3 space-y-1 text-white">
-                                    <li>TODO</li>
+                                    <ul class="list-disc list-inside mt-1">
+                                        <li>Privacy-preserving sound classification</li>
+                                        <li>Smart event monitoring</li>
+                                        <li>Urban planning and noise mapping</li>
+                                        <li>Identifying noise sources (music, crowds, traffic, etc.)</li>
+                                        <li>Threshold-based alerts without storing audio</li>
+                                    </ul>
                                 </ul>
                             </div>
                             <div>
-                                <span class="font-departure text-[#00316F]">Description:</span>
-                                <div class="p-3">TODO</div>
+                                <span class="font-departure text-[#fff]">Description:</span>
+                                <div class="p-3">
+                                    A dual-microphone Sound Sensor for contextual noise monitoring. When noise levels change, on-device Edge AI instantly classifies sources like machinery, music, or voices. All audio is processed locally and deleted immediately, providing actionable insight without
+                                    retaining data.
+                                </div>
                             </div>
                         </div>
                     </SensorCard>
@@ -151,19 +160,28 @@
 
             <div class="p-6 min-h-[30vh] w-[80%] m-auto">
                 <span class="font-departure text-primary text-xl">Why pick outdoor SPL?</span>
-                <div class="mx-auto bg-white rounded-xl">TODO</div>
+                <div class="mx-auto bg-white rounded-xl">
+                    Outdoor SPL provides accurate, real-world sound pressure data that reflects the true acoustic conditions of an environment. By capturing noise exactly where it occurs, it becomes an essential foundation for understanding environmental impact, assessing compliance, and improving
+                    long-term planning.
+                    <br />
+                    <br />
+                    When paired with our Edge AI classification pipeline, Outdoor SPL doesn’t just tell you how loud something was it helps reveal what caused it.
+                    <br />
+                    <br />
+                    Whether it’s traffic flow, construction machinery, or large crowd activity, you gain contextual insight that transforms raw decibel readings into actionable intelligence for cities, venues, and operational teams.
+                </div>
             </div>
         </div>
 
         <!-- MHT -->
         <div class="w-full">
-            <div class="py-5 grid gap-5 bg-[#FFC4C4F0] w-full">
+            <div class="py-5 grid gap-5 bg-[#35374B] w-full">
                 <span id="mht" class="m-auto text-white font-departure text-xl text-left w-[90%] pt-24">Wood Moisture Content sensor</span>
                 <div class="m-auto w-[80%] flex items-center justify-center">
                     <SensorCard fileLeft="mht-transparent.png" fileRight="mht.png" class="rounded-xl ">
                         <div class=" h-full p-3 text-white flex flex-wrap">
                             <div>
-                                <span class="font-departure text-red-800">Best for:</span>
+                                <span class="font-departure">Best for:</span>
                                 <ul class="list-disc pl-6 space-y-1 pt-1">
                                     <li>Monitoring moisture levels in timber constructions</li>
                                     <li>Preventing mold, rot, and structural damage in wooden buildings</li>
@@ -173,7 +191,7 @@
                             </div>
 
                             <div>
-                                <span class="font-departure text-red-800">Description:</span>
+                                <span class="font-departure">Description:</span>
                                 <div class="p-3">Sensors installed directly in timber to track moisture levels, combined with ambient RH/T sensors. They help prevent mold, rot, and structural issues, supporting timber construction and sustainable building research.</div>
                             </div>
                         </div>
@@ -192,7 +210,7 @@
                     </p>
 
                     <p class="text-slate-700 leading-relaxed mb-4">
-                        Installed directly into the wood by screwing them into the material, the sensors measure moisture inside the timber itself—offering far more relevant data than standard relative humidity (RH) and temperature (T) sensors alone. To achieve comprehensive monitoring, we typically
+                        Installed directly into the wood by screwing them into the material, the sensors measure moisture inside the timber itsel ffering far more relevant data than standard relative humidity (RH) and temperature (T) sensors alone. To achieve comprehensive monitoring, we typically
                         install these sensors within the building envelope, complemented by three additional RH/T sensors that track surrounding air conditions.
                     </p>
 
@@ -206,13 +224,13 @@
 
         <!-- Grafana -->
         <div class="w-full">
-            <div class="py-5 grid gap-5 bg-gray-200 w-full">
-                <span id="grafana" class="text-slate-700 font-departure text-xl text-left m-auto w-[90%] pt-24">Data Visualisation - Grafana Dashboard</span>
+            <div class="py-5 grid gap-5 bg-[#0F0E0E] w-full text-white">
+                <span id="grafana" class=" font-departure text-xl text-left m-auto w-[90%] pt-24">Data Visualisation - Grafana Dashboard</span>
                 <div class="m-auto w-[80%] flex items-center justify-center">
                     <SensorCard fileLeft="grafana-test.png" fileRight="sketch.svg" class="shadow bg-white rounded-xl " rightClass="object-contain">
-                        <div class=" h-full p-3 text-slate-700 flex flex-wrap">
+                        <div class=" h-full p-3 flex flex-wrap">
                             <div>
-                                <span class="font-departure text-[#48486F]">Data export:</span>
+                                <span class="font-departure">Data export:</span>
                                 <ul class="list-disc pl-8 pt-3 space-y-1">
                                     <li>CSV</li>
                                     <li>JSON</li>
@@ -220,7 +238,7 @@
                             </div>
 
                             <div>
-                                <span class="font-departure text-[#48486F]">Short Description:</span>
+                                <span class="font-departure">Short Description:</span>
                                 <div class="p-2">
                                     All sensors work with the software system, which combines open-source and custom solutions, using a high-performance time-series database for fast, interval-based queries.
                                     <div class="border border-green-600/85 bg-green-600/10 w-min py-1 px-2 text-nowrap rounded-full text-sm text-green-600">Open Source</div>
